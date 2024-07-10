d1db06353de2883f56236b49bc131d9a9d3fa9c8
Hamza Almhameed <143035357+Hamza-Almhameed@users.noreply.github.com>
2024-07-10 14:42:43 +0300

    Add files via upload

---
+++ JS/script.js
@@ -0,0 +1,7 @@
+var generate = document.getElementById("generate")
+var newPass = document.getElementById("newPassword")
+var characters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "D", "T", "U", "V", "W", "X", "Y", "Z", "_", "-", ")", "%", "&", "$", "_", "-", ")", "%", "&", "$"]
+
+generate.onclick = function() {
+    newPass.innerHTML = characters[Math.floor(Math.random() * characters.length)] + characters[Math.floor(Math.random() * characters.length)] + characters[Math.floor(Math.random() * characters.length)] + characters[Math.floor(Math.random() * characters.length)] + characters[Math.floor(Math.random() * characters.length)] + characters[Math.floor(Math.random() * characters.length)] + characters[Math.floor(Math.random() * characters.length)] + characters[Math.floor(Math.random() * characters.length)]
+}
